e4a47eaf311be4f6e0f261e7be26eca0c9273eae
ReygaFitra <regafitra12@gmail.com>
2023-04-17 22:17:37 +0700

    update user_repository

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/ReygaFitra/inc-final-project.git
 go 1.20
 
 require (
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/lib/pq v1.10.8 // indirect
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.8
 )
--- repository/user_repository.go
+++ repository/user_repository.go
@@ -1,8 +1,12 @@
 package repository
 
 import (
+	"database/sql"
+	"log"
+
 	"github.com/ReygaFitra/inc-final-project.git/model"
 )
+
 
 type UserRepository interface {
 	FindByID(id uint) (*model.Users, error)
@@ -11,4 +15,22 @@ type UserRepository interface {
 	Create(user *model.Users) error
 	Update(user *model.Users) error
 	Delete(user *model.Users) error
+}
+
+type UserRepo interface {
+	Register(newUser *model.Users) string
+}
+
+type userRepo struct {
+	db *sql.DB
+}
+
+func (r *userRepo) Create(newUser *model.Users) string {
+	query := "INSERT INTO (name, email, password, phone_number, address, balance) VALUES ($1, $2, $3, $4, $5, $6)"
+	_, err := r.db.Exec(query, newUser.Name, newUser.Email, newUser.Password, newUser.Phone_Number, newUser.Address, newUser.Balance) 
+	if err != nil {
+		log.Println(err.Error())
+		return "Failed Register"
+	}
+	return "Register Successfully"
 }
