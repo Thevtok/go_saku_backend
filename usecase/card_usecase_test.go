b5a4e10e3fa134903d986673f28716eb94a486f1
ReygaFitra <regafitra12@gmail.com>
2023-04-30 22:41:41 +0700

    update user_repo unit test

--- usecase/card_usecase_test.go
+++ usecase/card_usecase_test.go
@@ -1,65 +1,263 @@
 package usecase
 
-// import (
-// 	"github.com/ReygaFitra/inc-final-project.git/model"
+import (
+	"errors"
+	"testing"
 
-// 	"github.com/stretchr/testify/assert"
-// 	"github.com/stretchr/testify/mock"
-// 	"github.com/stretchr/testify/suite"
-// )
+	"github.com/ReygaFitra/inc-final-project.git/model"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/suite"
+)
 
-// var dummyCard = []model.Card{
-// 	{
-// 		UserID:         1,
-// 		CardType:       "BRI",
-// 		CardNumber:     "1234-5678-9101-3456",
-// 		ExpirationDate: "07/25",
-// 		CVV:            "123",
-// 	},
-// 	{
-// 		UserID:         2,
-// 		CardType:       "BCA",
-// 		CardNumber:     "4321-8765-9101-3456",
-// 		ExpirationDate: "03/26",
-// 		CVV:            "321",
-// 	},
+var dummyCard = []model.Card{
+	{
+		CardID: 		1,
+		UserID:         1,
+		CardType:       "BRI",
+		CardNumber:     "1234-5678-9101-3456",
+		ExpirationDate: "07/25",
+		CVV:            "123",
+	},
+	{
+		CardID: 		2,
+		UserID:         2,
+		CardType:       "BCA",
+		CardNumber:     "4321-8765-9101-3456",
+		ExpirationDate: "03/26",
+		CVV:            "321",
+	},
+}
+var dummyCardResponse = []model.CardResponse{
+	{
+		UserID:         1,
+		CardType:       "BRI",
+		CardNumber:     "1234-5678-9101-3456",
+		ExpirationDate: "07/25",
+		CVV:            "123",
+	},
+	{
+		UserID:         2,
+		CardType:       "BCA",
+		CardNumber:     "4321-8765-9101-3456",
+		ExpirationDate: "03/26",
+		CVV:            "321",
+	},
+}
+var dummyCardResponse1 = []*model.CardResponse{
+	{
+		UserID:         1,
+		CardType:       "BRI",
+		CardNumber:     "1234-5678-9101-3456",
+		ExpirationDate: "07/25",
+		CVV:            "123",
+	},
+	{
+		UserID:         2,
+		CardType:       "BCA",
+		CardNumber:     "4321-8765-9101-3456",
+		ExpirationDate: "03/26",
+		CVV:            "321",
+	},
+}
+
+type cardRepoMock struct {
+	mock.Mock
+}
+
+// type CardRepository interface {
+// 	GetAll() any
+// 	GetByUserID(id uint) ([]*model.CardResponse, error)
+// 	GetByCardID(id uint) (*model.Card, error)
+// 	Create(id uint, newCard *model.CardResponse) (any, error)
+// 	Update(card *model.Card) string
+// 	DeleteByUserID(id uint) string
+// 	DeleteByCardID(cardID uint) error
 // }
 
-// type cardRepoMock struct {
-// 	mock.Mock
-// }
+func (u *cardRepoMock) GetAll() any {
+	args := u.Called()
+	if args.Get(0) == nil {
+		return nil
+	}
+	return dummyCard
+}
 
-// func (m *cardRepoMock) GetAll() []model.Card {
-// 	args := m.Called()
-// 	return args.Get(0).([]model.Card)
-// }
+func (u *cardRepoMock) GetByUserID(id uint) ([]*model.CardResponse, error) {
+	args := u.Called(id)
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).([]*model.CardResponse), nil
+}
 
-// func (m *cardRepoMock) FindCardByUserID(id uint) ([]model.Card, error) {
-// 	args := m.Called(id)
-// 	return args.Get(0).([]model.Card), args.Error(1)
-// }
+func (u *cardRepoMock) GetByCardID(id uint) (*model.Card, error) {
+	args := u.Called(id)
+	if args.Get(0) == nil {
+		return nil, errors.New("card account not found")
+	}
+	return args.Get(0).(*model.Card), nil
+}
 
-// func (m *cardRepoMock) FindCardByCardID(id uint) (*model.Card, error) {
-// 	args := m.Called(id)
-// 	return args.Get(0).(*model.Card), args.Error(1)
-// }
+func (u *cardRepoMock) Create(id uint, newCard *model.CardResponse) (any, error) {
+	args := u.Called(id, newCard)
+	if args.Get(0) == nil {
+		return nil, errors.New("failed to create data")
+	}
+	return dummyCardResponse, nil
+}
 
-// func (m *cardRepoMock) Create(id uint, newCard *model.CardResponse) (*model.Card, error) {
-// 	args := m.Called(id, newCard)
-// 	return args.Get(0).(*model.Card), args.Error(1)
-// }
+func (u *cardRepoMock) Update(card *model.Card) string {
+	args := u.Called(card)
+	if args.Get(0) == nil {
+		return "failed to update Card Account"
+	}
+	return "Card Account updated Successfully"
+}
 
-// func (m *cardRepoMock) Update(card *model.Card) error {
-// 	args := m.Called(card)
-// 	return args.Error(0)
-// }
+func (u *cardRepoMock) DeleteByUserID(id uint) string {
+	args := u.Called(id)
+	if args.Get(0) == nil {
+		return "failed to delete Card Account"
+	}
+	return "All Card Account deleted Successfully"
+}
 
-// func (m *cardRepoMock) DeleteByUserID(userID uint) error {
-// 	args := m.Called(userID)
-// 	return args.Error(0)
-// }
+func (u *cardRepoMock) DeleteByCardID(cardID uint) error {
+	args := u.Called(cardID)
+	if args.Get(0) != nil {
+		return errors.New("failed to delete data")
+	}
+	return nil
+}
 
-// func (m *cardRepoMock) DeleteByCardID(cardID uint) error {
-// 	args := m.Called(cardID)
-// 	return args.Error(0)
-// }
+type CardUsecaseTestSuite struct {
+	cardaccRepoMock *cardRepoMock
+	suite.Suite
+}
+
+func (suite *CardUsecaseTestSuite) TestFindAllCard_Success() {
+	cardUsecase := NewCardUsecase(suite.cardaccRepoMock)
+	suite.cardaccRepoMock.On("GetAll").Return(dummyCard)
+	res := cardUsecase.FindAllCard()
+	assert.NotNil(suite.T(), res)
+	assert.Equal(suite.T(), dummyCard, res)
+}
+func (suite *CardUsecaseTestSuite) TestFindAllCard_Failed() {
+	cardUsecase := NewCardUsecase(suite.cardaccRepoMock)
+	suite.cardaccRepoMock.On("GetAll").Return(nil)
+	res := cardUsecase.FindAllCard()
+	assert.Nil(suite.T(), res)
+	assert.Empty(suite.T(), res)
+}
+
+func (suite *CardUsecaseTestSuite) TestFindCardByUserID_Success() {
+	userID := uint(1)
+	cardAcc := dummyCardResponse1
+	cardUsecase := NewCardUsecase(suite.cardaccRepoMock)
+	suite.cardaccRepoMock.On("GetByUserID", userID).Return(cardAcc, nil)
+	result, err := cardUsecase.FindCardByUserID(userID)
+	assert.NoError(suite.T(), err)
+	assert.Equal(suite.T(), cardAcc, result)
+}
+func (suite *CardUsecaseTestSuite) TestFindCardByUserID_Failed() {
+	userID := uint(1)
+	expectedError := errors.New("failed to get card account")
+	cardUsecase := NewCardUsecase(suite.cardaccRepoMock)
+	suite.cardaccRepoMock.On("GetByUserID", userID).Return(nil, expectedError)
+	result, err := cardUsecase.FindCardByUserID(userID)
+	assert.Error(suite.T(), err)
+	assert.Nil(suite.T(), result)
+}
+
+func (suite *CardUsecaseTestSuite) TestFindCardByCardID_Success() {
+	userID := uint(1)
+	cardAcc := &dummyCard[0]
+	cardUsecase := NewCardUsecase(suite.cardaccRepoMock)
+	suite.cardaccRepoMock.On("GetByCardID", userID).Return(cardAcc, nil)
+	result, err := cardUsecase.FindCardByCardID(userID)
+	assert.NoError(suite.T(), err)
+	assert.Equal(suite.T(), cardAcc, result)
+}
+func (suite *CardUsecaseTestSuite) TestFindCardByCardID_Failed() {
+	userID := uint(1)
+	expectedError := errors.New("card account not found")
+	cardUsecase := NewCardUsecase(suite.cardaccRepoMock)
+	suite.cardaccRepoMock.On("GetByCardID", userID).Return(nil, expectedError)
+	result, err := cardUsecase.FindCardByCardID(userID)
+	assert.Error(suite.T(), err)
+	assert.Nil(suite.T(), result)
+}
+
+func (suite *CardUsecaseTestSuite) TestRegister_Success() {
+	userID := uint(1)
+	cardUsecaseMock := NewCardUsecase(suite.cardaccRepoMock)
+	suite.cardaccRepoMock.On("Create", userID, &dummyCardResponse[0]).Return(dummyCardResponse, nil)
+	result, err := cardUsecaseMock.Register(userID, &dummyCardResponse[0])
+	assert.NotNil(suite.T(), result)
+	assert.Nil(suite.T(), err)
+}
+func (suite *CardUsecaseTestSuite) TestRegister_Failed() {
+	userID := uint(1)
+	expectedError := errors.New("failed to create data")
+	cardUsecaseMock := NewCardUsecase(suite.cardaccRepoMock)
+	suite.cardaccRepoMock.On("Create", userID, &dummyCardResponse[0]).Return(nil, expectedError)
+	result, err := cardUsecaseMock.Register(userID, &dummyCardResponse[0])
+	assert.Nil(suite.T(), result)
+	assert.NotNil(suite.T(), err)
+}
+
+func (suite *CardUsecaseTestSuite) TestEdit_Success() {
+	cardAcc := &dummyCard[0]
+	cardUsecase := NewCardUsecase(suite.cardaccRepoMock)
+	suite.cardaccRepoMock.On("Update", cardAcc).Return("Card Account updated Successfully")
+	result := cardUsecase.Edit(cardAcc)
+	assert.NotNil(suite.T(), result)
+}
+func (suite *CardUsecaseTestSuite) TestEdit_Failed() {
+	cardAcc := &dummyCard[0]
+	cardUsecase := NewCardUsecase(suite.cardaccRepoMock)
+	suite.cardaccRepoMock.On("Update", cardAcc).Return("Failed to Update Card Account")
+	err := cardUsecase.Edit(cardAcc)
+	assert.NotNil(suite.T(), err)
+}
+
+func (suite *CardUsecaseTestSuite) TestUnregAll_Success() {
+	userID := uint(1)
+	cardAcc := dummyCard
+	cardUsecase := NewCardUsecase(suite.cardaccRepoMock)
+	suite.cardaccRepoMock.On("DeleteByUserID", userID).Return("All Card Account deleted Successfully")
+	result := cardUsecase.UnregALL(&cardAcc[0])
+	assert.NotNil(suite.T(), result)
+}
+func (suite *CardUsecaseTestSuite) TestUnregAll_Failed() {
+	userID := uint(1)
+	cardAcc := dummyCard
+	cardUsecase := NewCardUsecase(suite.cardaccRepoMock)
+	suite.cardaccRepoMock.On("DeleteByUserID", userID).Return("Failed to delete Card Account")
+	err := cardUsecase.UnregALL(&cardAcc[0])
+	assert.NotNil(suite.T(), err)
+}
+
+func (suite *CardUsecaseTestSuite) TestUnregByCardID_Success() {
+	cardID := uint(1)
+	cardUsecase := NewCardUsecase(suite.cardaccRepoMock)
+	suite.cardaccRepoMock.On("DeleteByCardID", cardID).Return(nil)
+	err := cardUsecase.UnregByCardID(cardID)
+	assert.NoError(suite.T(), err)
+}
+func (suite *CardUsecaseTestSuite) TestUnregByCardID_Failed() {
+	cardID := uint(1)
+	cardUsecase := NewCardUsecase(suite.cardaccRepoMock)
+	suite.cardaccRepoMock.On("DeleteByCardID", cardID).Return(errors.New("failed to delete data"))
+	err := cardUsecase.UnregByCardID(cardID)
+	assert.EqualError(suite.T(), err, "failed to delete data")
+}
+
+func (suite *CardUsecaseTestSuite) SetupTest() {
+	suite.cardaccRepoMock = new(cardRepoMock)
+}
+
+func TestCardAccTestSuite(t *testing.T) {
+	suite.Run(t, new(CardUsecaseTestSuite))
+}
