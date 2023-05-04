497079cfe06996e4a1cb2c9966e67a38dfd8b130
thevtok <alfarizifikri2406@gmail.com>
2023-05-04 08:44:57 +0700

    test

--- main.go
+++ main.go
@@ -1,15 +1,10 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/ReygaFitra/inc-final-project.git/delivery"
 )
 
 func main() {
-	http.HandleFunc("/", Handler)
-	delivery.RunServer()
-}
-func Handler(w http.ResponseWriter, r *http.Request) {
+
 	delivery.RunServer()
 }
